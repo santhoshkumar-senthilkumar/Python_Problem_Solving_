b3791918e884b0031dc0359af46d3b1d81226e72
SANTHOSH KUMAR S <santhoshgomathi2020@gmail.com>
2026-08-18 18:34:42 +0530

    Update Bus_Ticket_Function.py

--- Python_day_9/Bus_Ticket_Function.py
+++ Python_day_9/Bus_Ticket_Function.py
@@ -1,19 +1,19 @@
 def func():
- a=input("Enter a Gender : ").upper()
- if a=="MALE":
- age = int(input("Enter a age : "))
- if age >=60 or age <=12 :
- conductor = input("I Am Your Friend means give yes : ").upper()
- if conductor == "YES" :
- print("Your ticket is free")
- else :
- print("Your ticket is half price")
- else :
- print("You Want to pay full Ticket price")
- elif a=="FEMALE" :
- print("Your ticket is free ")
- else :
- print("Invalid entry")
- func()
-
+    a=input("Enter a Gender : ").upper()
+    if a=="MALE":
+        age = int(input("Enter a age : "))
+        if age >=60 or age <=12 :
+            conductor = input("I Am Your Friend means give yes : ").upper()
+            if conductor == "YES" :
+             print("Your ticket is free")
+            else :
+                print("Your ticket is half price")
+        else :
+         print("You Want to pay full Ticket price")
+    elif a=="FEMALE" :
+        print("Your ticket is free ")
+    else :
+        print("Invalid entry")
+        func()
+ 
 func()
