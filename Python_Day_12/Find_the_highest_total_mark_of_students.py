781b7b58b95a562c4a134b0d55055a7a19c10a52
SANTHOSH KUMAR S <santhoshgomathi2020@gmail.com>
2026-08-21 19:43:03 +0530

    Update Find_the_highest_total_mark_of_students.py

--- Python_Day_12/Find_the_highest_total_mark_of_students.py
+++ Python_Day_12/Find_the_highest_total_mark_of_students.py
@@ -1,1 +1,19 @@
-
+a=int(input("Enter the number of students : "))
+b=int(input("Enter the number of subjects : "))
+students ={}
+Totalmarks=[]
+for i in range(a):
+    name = input("Enter the name : ")
+    if name not in students :
+        marks=[]
+        total=0
+        for i in range(b):
+           marks.append(int(input("Enter the marks : ")))
+           total += marks[i]
+    students[name]=marks
+    Totalmarks.append({"name": name ,"total" : total})
+Highest=Totalmarks[0]
+for i in Totalmarks:
+    if Highest["total"]<i["total"]:
+        Highest=i 
+print(Highest)
