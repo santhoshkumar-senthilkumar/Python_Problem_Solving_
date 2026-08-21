a212d55ccbaf5130394a22c1e7bccb827bb8ead9
SANTHOSH KUMAR S <santhoshgomathi2020@gmail.com>
2026-08-21 19:42:20 +0530

    Update Find_employees_by_using_a_id.py

--- Python_Day_12/Find_employees_by_using_a_id.py
+++ Python_Day_12/Find_employees_by_using_a_id.py
@@ -1,1 +1,15 @@
-
+a=int(input("Enter the number of employees : "))
+employees=[]
+idstart = 100
+for i in range(a):
+    b={}
+    b["Id"] = idstart
+    b["Name"]=input(f"Your id is {idstart} Enter the name : ")
+    b["salary"]=int(input("Enter the salary : "))
+    employees.append(b)
+    idstart  += 1
+search = int(input("Enter the ID to Search : "))
+for i in employees:
+    if i["Id"]==search:
+        print(i)
+        break;
