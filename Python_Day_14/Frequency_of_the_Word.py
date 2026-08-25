72e9f43d0cae91529ab31a408e1255c3ca639f60
SANTHOSH KUMAR S <santhoshgomathi2020@gmail.com>
2026-08-25 17:49:59 +0530

    Implement word frequency counter
    Added a script to count the frequency of words input by the user.

--- Python_Day_14/Frequency_of_the_Word.py
+++ Python_Day_14/Frequency_of_the_Word.py
@@ -1,1 +1,18 @@
 
+# Frequency_of_The_Word
+
+a = int(input("Enter the number of words: "))
+b = []
+
+for i in range(a):
+    b.append(input())
+
+c = {}
+
+for i in b:
+    if i not in c:
+        c[i] = 1
+    elif i in c:
+        c[i] += 1
+
+print(c)
