f8bb3508db1619c0ffa613e1420ac62f872b893e
SANTHOSH KUMAR S <santhoshgomathi2020@gmail.com>
2026-09-01 19:31:31 +0530

    Update Find_total_sales_of_product.py

--- Python_Day_17/Find_total_sales_of_product.py
+++ Python_Day_17/Find_total_sales_of_product.py
@@ -1,1 +1,20 @@
-
+sales = [
+"Laptop 50000",
+"Phone 30000",
+"Laptop 45000",
+"Tablet 20000",
+"Phone 25000",
+"Laptop 55000"
+]
+total ={}
+for i in sales:
+    d = i.split(" ")
+    if d[0] not in total:
+        total[d[0]] = int(d[1])
+    elif d[0] in total:
+       total[d[0]] += int(d[1])
+high =0
+for i in total:
+    if total[i]>high:
+        high= total[i]
+print(high)
