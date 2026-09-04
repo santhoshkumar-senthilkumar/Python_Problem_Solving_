c1289435d6afb7dc09bce21f1fb4e1538fbe7a20
SANTHOSH KUMAR S <santhoshgomathi2020@gmail.com>
2026-09-04 19:01:16 +0530

    Update Version_history_tracker.py

--- Python_Day_20/Version_history_tracker.py
+++ Python_Day_20/Version_history_tracker.py
@@ -1,1 +1,17 @@
-
+logs = [
+    "file1 v1",
+    "file2 v1",
+    "file1 v2",
+    "file3 v1",
+    "file1 v3",
+    "file2 v2"
+]
+d ={}
+for i in logs:
+    s = i.split(" ")
+    if s[0] not in d:
+        d[s[0]]=[]
+        d[s[0]].append(s[1])
+    elif s[0] in d:
+        d[s[0]].append(s[1])
+print(d)
